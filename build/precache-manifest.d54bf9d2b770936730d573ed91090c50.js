35de684a65ab0c7dfe67d7f2efa5e898833c89e3
vladukan <vladukan@gmail.com>
2020-05-06 13:05:33 +0300

    upload

---
+++ build/precache-manifest.d54bf9d2b770936730d573ed91090c50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3633b117d206fbe753d3f56cdd7e73e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddfa5fcbe55d909adf5a",
+    "url": "/static/css/main.75a0096e.chunk.css"
+  },
+  {
+    "revision": "1453a2ce33d49f808976",
+    "url": "/static/js/2.e48285f1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e48285f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddfa5fcbe55d909adf5a",
+    "url": "/static/js/main.c752060b.chunk.js"
+  },
+  {
+    "revision": "f1dee849d19ffedb49e9",
+    "url": "/static/js/runtime-main.c6e75796.js"
+  }
+]);
